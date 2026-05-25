7c7d835ea3a402f8392d0bd49b3f900d9f22b1b2
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-05-25 12:14:07 +0900

    0.1 and add test3

---
+++ test3.cpp
@@ -0,0 +1,28 @@
+// test3.cpp --- WonShellExecute tests
+// Author: katahiromz
+// License: MIT
+#include <windows.h>
+#include <shlobj.h>
+#include <stdio.h>
+#include <string>
+#include "shlexec.h"
+
+int main(void)
+{
+    HWND hwnd = NULL;
+    LPITEMIDLIST pidl;
+    HRESULT hr = SHGetSpecialFolderLocation(hwnd, CSIDL_PRINTERS, &pidl);
+    if (FAILED(hr))
+        return 1;
+
+    SHELLEXECUTEINFOW sei = {sizeof(sei)};
+    sei.hwnd = hwnd;
+    sei.lpVerb = L"open";
+    sei.nShow = SW_SHOWNORMAL;
+    sei.fMask = SEE_MASK_IDLIST;
+    sei.lpIDList = pidl;
+    BOOL bOK = WonShellExecuteExW(&sei);
+
+    CoTaskMemFree(pidl);
+    return bOK ? 0 : 1;
+}
